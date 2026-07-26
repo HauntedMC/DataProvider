d459f1a0e5d65d3461cb3252ee580e57bbc91144
remdui <remyduijsens@gmail.com>
2026-07-26 12:36:49 +0200

    Bump version to v3.1.1 for release

--- dataprovider-platform-velocity/src/main/java/nl/hauntedmc/dataprovider/platform/velocity/VelocityDataProvider.java
+++ dataprovider-platform-velocity/src/main/java/nl/hauntedmc/dataprovider/platform/velocity/VelocityDataProvider.java
@@ -25,7 +25,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "dataprovider",
         name = "DataProvider",
-        version = "3.1.0",
+        version = "3.1.1",
         description = "A cross-platform data provider plugin.",
         authors = {"HauntedMC"}
 )
